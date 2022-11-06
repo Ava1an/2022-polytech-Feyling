8dbadb87964bdc5c484b233a83ef825402d1d01f
Ava1an <zomby125_3@mail.ru>
2022-11-06 20:34:02 +0300

    feat add programm

--- 03_cpp/gcd/gcd.cpp
+++ 03_cpp/gcd/gcd.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
 #include <cassert>
 
-int gcd(int a, int b) {
-	if (a < 0) a *= -1;
-	if (b < 0) b *= -1;
-	
-	if (a == 0) return b;
-	if (b == 0) return a;
-	
-	if (a % b == 0) return b;
-	if (b % a == 0) return a;
+using namespace std;
 
-	if (a > b) return gcd(a % b, b);
-	return gcd(a, b % a);
+int gcd(int a, int b)
+{
+	if (a < 0)
+	{
+		a *= -1;
+	}
+	if (b < 0)
+	{
+		b *= -1;
+	}
+	while ((a != 0) && (b != 0))
+	{
+		if (a > b)
+		{
+			a = a - b;
+		}
+		else
+		{
+			b = b - a;
+		}
+	}
+	if (a != 0)
+	{
+		cout << a << endl;
+		return a;
+	}
+	else
+	{
+		cout << b << endl;
+		return b;
+	}
 }
 
 int main() {
+	gcd(36, 48);
 	assert(gcd(0, 5) == 5);
 	assert(gcd(9, 0) == 9);
 	assert(gcd(48, 64) == 16);
